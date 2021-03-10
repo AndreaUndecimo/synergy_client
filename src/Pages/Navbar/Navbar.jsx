988777c0e20eb1ed493143906279e8f7b5c8db55
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-03-10 19:05:07 +0100

    add(FE): navbar

---
+++ src/Pages/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import './Navbar.scss';
+
+const Navbar = () => {
+  const toggleLoginForm = (e) => {
+    document.querySelector('.register_wrapper').classList.add('show');
+  };
+
+  return (
+    <>
+      <div className='register_wrapper'>
+        <Register />
+      </div>
+      <div className='login_wrapper'>
+        <Login />
+      </div>
+      <div className='navbar_wrapper'>
+        <div className='logo'>
+          <h1>SYNERGY</h1>
+        </div>
+        <div className='user_actions'>
+          <button id='join_btn' onClick={toggleLoginForm}>
+            Join
+          </button>
+          <button id='signup_btn' onClick={toggleLoginForm}>
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
